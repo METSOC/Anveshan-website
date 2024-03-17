1cdb130c0147a13764e542270b96edc9248cfd52
ShaunAryan28 <123452148+ShaunAryan28@users.noreply.github.com>
2024-03-17 19:04:31 +0530

    contact us

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Registration from "./pages/Registration";
 import Homepage from "./pages/Homepage";
 import Events from "./pages/Events";
-
+import ContactUs from "./components/ContactUs/ContactUs";
 function App() {
   return (
     <>
@@ -11,6 +11,7 @@ function App() {
         <Route path="/login" element={<Registration type="login" />} />
         <Route path="/signup" element={<Registration type="signup" />} />
         <Route path="/events" element={<Events/>} />
+        <Route path="/contactus" element={<ContactUs/>} />
       </Routes>
     </>
   );
